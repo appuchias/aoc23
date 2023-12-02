64b20618121674f8656d786d423c1fee5594597e
Appuchia <appuchia@appu.ltd>
2023-12-02 13:11:47 +0100

    Improve adday

--- addday.sh
+++ addday.sh
@@ -2,7 +2,30 @@
 
 echo "#! /usr/bin/env python3
 
-# Advent of Code 23 - Day $1" > $1.py
+# Advent of Code 23 - Day $1
+
+from pathlib import Path
+
+from tools import readfile
+
+
+def p1(contents: list[str]) -> int:
+    ...
+
+
+def p2(contents: list[str]) -> int:
+    ...
+
+
+if __name__ == "__main__":
+    fp = Path("inputs/$1.txt")
+    fp = Path("samples/$1-1.txt")
+    
+    contents = readfile(fp)
+
+    print(p1(contents))
+    print(p2(contents))
+" > $1.py
 
 chmod +x $1.py
 
