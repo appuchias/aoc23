ca7603a7021ec48dde0815c93406dc06d53a57eb
Appuchia <appuchia@appu.ltd>
2023-12-04 19:28:51 +0100

    04: P2 runs. Output too high

--- 04.py
+++ 04.py
@@ -57,36 +57,50 @@ def p2(contents: list[str]) -> int:
     base_cards = dict(
         zip(range(1, len(my_numbers) + 1), list(zip(winners, my_numbers)))
     )
-    # Diccionario: {id: (winners, my_numbers)}
+    # Dict: {id: (winners, my_numbers)}
 
-    card_count = 0
-    cards = list(base_cards.keys())
+    # Dict: {id: count}
+    cards = {k: 1 for k in base_cards.keys()}
+    total_card_count = 0
 
     while len(cards) > 1:
-        cards_to_add = list()
-        for card_id in cards:
-            cards.remove(card_id)
-            card_count += 1
+        cards_to_add = dict()
 
+        # Iterate over the card ids
+        for card_id, card_count in cards.items():
+            total_card_count += card_count
+
+            # Get points for the card id
             points = points_for_card(base_cards[card_id])
 
             if points:
+                # Get the card ids that need to be added
                 new_game_ids = range(card_id + 1, card_id + points + 1)
+
+                # Prevent out of bounds
                 new_game_ids = filter(lambda x: x <= len(base_cards), new_game_ids)
-                cards_to_add.extend(new_game_ids)
 
-        cards.extend(cards_to_add)
+                # Set new card counts
+                for new_game_id in new_game_ids:
+                    if new_game_id not in cards_to_add:
+                        cards_to_add[new_game_id] = 0
 
-        # print(" ", card_count, len(cards))
+                    cards_to_add[new_game_id] += card_count
 
-    return card_count
+        cards = cards_to_add
+
+        print("", total_card_count, len(cards_to_add))
+
+    return total_card_count
 
 
 if __name__ == "__main__":
     fp = Path("inputs/04.txt")
-    fp = Path("samples/04-1.txt")
+    # fp = Path("samples/04-1.txt")
 
     contents = readfile(fp)
 
     print(p1(contents))
     print(p2(contents))
+
+# x < 83146687209054963286543604131605810512
