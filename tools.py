5d6d6dad5a0c01d3e8a8fdb9a13be60a5b68da50
Appuchia <appuchia@appu.ltd>
2023-12-06 14:13:47 +0100

    feat: Add Point class to tools

--- tools.py
+++ tools.py
@@ -3,7 +3,30 @@
 # Advent of Code 23 - Tools
 # Appuchia <appuchia@appu.ltd>
 
+from dataclasses import dataclass
 from pathlib import Path
+
+
+@dataclass(frozen=True)
+class Point:
+    x: int
+    y: int
+
+    def get_neighbors(self) -> list["Point"]:
+        return [
+            Point(self.x - 1, self.y),
+            Point(self.x + 1, self.y),
+            Point(self.x, self.y - 1),
+            Point(self.x, self.y + 1),
+        ]
+
+    def get_surronding(self) -> list["Point"]:
+        return self.get_neighbors() + [
+            Point(self.x - 1, self.y - 1),
+            Point(self.x + 1, self.y - 1),
+            Point(self.x - 1, self.y + 1),
+            Point(self.x + 1, self.y + 1),
+        ]
 
 
 def readfile(fp: Path) -> list[str]:
