b230b250f9fe8d75adcd6ac48d4e5aee66e35cb6
Appuchia <appuchia@appu.ltd>
2023-12-09 02:01:37 +0100

    08: Part 2 didn't finish in over 12h of processing. Will retry

--- 08.py
+++ 08.py
@@ -36,7 +36,23 @@ def p1(contents: list[str]) -> int:
 
 
 def p2(contents: list[str]) -> int:
-    ...
+    instructions, reference = parse_input(contents)
+
+    # check = lambda v: lambda k: k[2] == v  # check(n) -> lambda k: k[2] == n
+    keys = list(filter(lambda k: k[2] == "A", reference.keys()))
+
+    i = 0  # instruction pointer
+    while not all(k[2] == "Z" for k in keys):
+        newkeys = list()
+
+        for key in keys:
+            idx = instructions[i % len(instructions)]
+            newkeys.append(reference[key][idx])
+
+        i += 1
+        keys = newkeys
+
+    return i
 
 
 if __name__ == "__main__":
@@ -46,4 +62,8 @@ if __name__ == "__main__":
     contents = readfile(fp)
 
     print(p1(contents))  # x=17621
+
+    # fp = Path("samples/08-2.txt")
+    contents = readfile(fp)
+
     print(p2(contents))
